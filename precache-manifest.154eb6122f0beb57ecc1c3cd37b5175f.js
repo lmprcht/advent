4c5ec788cc3d85e77ad1b36fdf40fded32e81464
Dario Lamprecht <dario@lmprcht.de>
2022-11-30 21:03:03 +0100

    Updates

---
+++ precache-manifest.154eb6122f0beb57ecc1c3cd37b5175f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c11cd50b535c55906fa7efc305b4269",
+    "url": "/advent/index.html"
+  },
+  {
+    "revision": "0ca045e2c254f47aa7f4",
+    "url": "/advent/static/js/2.c635b16e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/advent/static/js/2.c635b16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5bf34fee9e8172dbbd",
+    "url": "/advent/static/js/main.541cb4c1.chunk.js"
+  },
+  {
+    "revision": "ce3a0813bb47c553e9be",
+    "url": "/advent/static/js/runtime-main.587eeb21.js"
+  },
+  {
+    "revision": "51dc08ce3286dab7c943ebd1bffb219a",
+    "url": "/advent/static/media/1.51dc08ce.jpg"
+  },
+  {
+    "revision": "8c998444db41358bc1269a11c4afae21",
+    "url": "/advent/static/media/10.8c998444.jpg"
+  },
+  {
+    "revision": "57c1030cb1368e5e4617d1da12e7d4ab",
+    "url": "/advent/static/media/2.57c1030c.jpg"
+  },
+  {
+    "revision": "bef801ea6cbf9a4a26b117c9285039d9",
+    "url": "/advent/static/media/3.bef801ea.jpg"
+  },
+  {
+    "revision": "db274fb582edddac0f801c2aa8f2e75a",
+    "url": "/advent/static/media/4.db274fb5.jpg"
+  },
+  {
+    "revision": "8e02ef9f88ee9897ef2a2a8b020ddc83",
+    "url": "/advent/static/media/5.8e02ef9f.jpg"
+  },
+  {
+    "revision": "afdbae96d89cd38e5ba205667cf30597",
+    "url": "/advent/static/media/6.afdbae96.jpg"
+  },
+  {
+    "revision": "c0f5972b230a204a00fa9b18cf7fa0bf",
+    "url": "/advent/static/media/7.c0f5972b.jpg"
+  },
+  {
+    "revision": "99db5f498642e4b05118da1226147048",
+    "url": "/advent/static/media/8.99db5f49.jpg"
+  },
+  {
+    "revision": "e5bb22b4c0af866bac1673da512b4201",
+    "url": "/advent/static/media/9.e5bb22b4.jpg"
+  },
+  {
+    "revision": "13a04aaf7aaaa5f4b06969e4ec4095ec",
+    "url": "/advent/static/media/anything_you_can_do_i_can_do_bleeding.13a04aaf.png"
+  },
+  {
+    "revision": "15ec96557cfb9d34bcdf487a20957eba",
+    "url": "/advent/static/media/calendar_backdrop.15ec9655.jpg"
+  },
+  {
+    "revision": "727453ae719b745d9f471d95dee5b130",
+    "url": "/advent/static/media/delica_ela_en_paz.727453ae.png"
+  },
+  {
+    "revision": "a87ae579554725d940660ae6ade656d0",
+    "url": "/advent/static/media/door_backdrop.a87ae579.jpg"
+  },
+  {
+    "revision": "781a1276ad0c7d42838e847ad2d4f96b",
+    "url": "/advent/static/media/feminism_symbol.781a1276.svg"
+  }
+]);
